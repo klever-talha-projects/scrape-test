fdeb523dacda45ed3ae855ad5003ce6647717751
klever-talha-projects <97839278+klever-talha-projects@users.noreply.github.com>
2023-10-14 10:47:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,34 @@ async function linkScrape(articleNum) {
     await page.setViewport({ width: 350, height: 800 });
     await page.setRequestInterception(true);
     page.on('request', (req) => {
-        if(req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font'){
+         var blockedURLs = [
+            "https://www.ikea.com/5sI96zHfk_/6kCCCZE4/Y6/O1f9kfOutO/fQEkCFEB/PhoFP/VF8XVsB",
+            "https://web-api.ikea.com/episod/inhouseEvent",
+            "https://s.yimg.com",
+            "https://www.google-analytics.com",
+            "https://icsp.ingka.ikea.com",
+            "https://favs.oneweb.ingka.com/graphql",
+            "https://cm.teads.tv/v2/advertiser",
+            "https://cdn.cookielaw.org/scripttemplates/202301.2.0/otBannerSdk.js",
+            "https://www.ikea.com/akam/13/pixel_72626eaa",
+            "https://cdn.cookielaw.org/consent/0125d2c5-0ef9-4613-a2b3-84d6ed780002/0125d2c5-0ef9-4613-a2b3-84d6ed780002.json",
+            "https://geolocation.onetrust.com/cookieconsentpub/v1/geo/location",
+            "https://api.ingka.ikea.com/guest/token",
+            "https://www.ikea.com/es/es/favs-agent/agent/agent-aacd9324.js",
+            "https://www.ikea.com/es/es/product-lists/modal~dropdown-1711.js",
+            "https://www.ikea.com/es/es/product-lists/dropdown-1711.js",
+            "https://www.ikea.com/es/es/product-lists/modal-1711.js",
+            "https://www.ikea.com/es/es/recommendations/panels/rec-add-to-cart.ebdcc721.chunk.js",
+            "https://www.ikea.com/es/es/recommendations/panels/rec_non_initial.f65acdd3.chunk.js",
+            "https://www.ikea.com/es/es/product-lists/compare-1711.js",
+            "https://www.ikea.com/es/es/recommendations/panels/rec-init-mount-panels.8eb7ff4f.chunk.js",
+            "https://www.ikea.com/es/es/static/ikea-logo.f7d9229f806b59ec64cb.svg",
+            "https://www.ikea.com/es/es/recommendations/panels/rec_vendors.f8ea59e2.chunk.js",
+            "https://www.ikea.com/es/es/loyalty/asset-manifest.json",
+            "https://www.ikea.com/es/es/recommendations/panels/rec_vendors_preact.9c28c032.chunk.js",
+            "https://www.ikea.com/es/es/recommendations/panels/rec_vendors_ingka.d014595b.chunk.js"
+          ];
+        if(req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || blockedURLs.includes(req.url())){
             req.abort();
         }
         else {
